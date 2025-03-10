8fbdd2faf6ff1d928902d1b86f65b5d4a27cd842
Ysaiharshith <y.saiharshith.cris@gmail.com>
2025-03-10 12:31:43 +0530

    new push

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('DOMContentLoaded', function() {
     var options1 = {
-      strings: ["FrontEnd Developer","Designer", "Web Developer", "Freelancer"],
+      strings: ["FrontEnd Developer","Designer", "Web Developer"],
       typeSpeed: 50,
       backSpeed: 25,
       backDelay: 1000,
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
     };
   
     var options2 = {
-      strings: ["FrontEnd Developer","Designer", "Web Developer", "Freelancer"],
+      strings: ["FrontEnd Developer","Designer", "Web Developer"],
       typeSpeed: 50,
       backSpeed: 25,
       backDelay: 1000,
@@ -24,4 +24,20 @@ document.addEventListener('DOMContentLoaded', function() {
     var typed1 = new Typed("#typed1", options1);
     var typed2 = new Typed("#typed2", options2);
   });
+
+document.getElementById("resume").addEventListener("click", function () {
+    
+  const link = document.createElement("a");
+  
+  link.href = "assets/resume.pdf"; 
+  
+  link.download = "My_Resume.pdf"; 
+  
+  document.body.appendChild(link);
+  
+  link.click();
+  
+  document.body.removeChild(link);
+});
+
   
